7b9af35b431ab4f1c8cc437f1004e47009d52725
Reza <reza@localhost.localdomain>
2019-05-01 22:13:10 +0700

    add database event

---
+++ database/migrations/2019_04_30_150652_create_event_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nama', 50);
+            $table->string('foto');
+            $table->string('deskripsi', 1000);
+            $table->string('alamat', 100);
+            $table->dateTime('tgl_mulai');
+            $table->dateTime('tgl_akhir');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event');
+    }
+}
